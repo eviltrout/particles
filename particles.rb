a7db4e399e9b799f436965903c079e00621f1123
Evil Trout <eviltrout@forumwarz.com>
2009-09-11 21:51:05 -0400

    First commit, including source, images and README.

--- particles.rb
+++ particles.rb
@@ -8,14 +8,19 @@ end
 require 'gosu'
 include Gosu
 
+# Our window size
 module Dimensions
   Width, Height = 1280, 720
 end
 
+# Our layers for drawing.
 module ZOrder
-  Background, Fuzzy = *0..1
+  Background, Shadow, Shape, Highlight = *0..3
 end
 
+# One particle is basically a sprite, that moves itself by speed every tick along an angle.
+# It also has a concept of decay, in how quickly the particle dies. In this case, the draw
+# command takes into account decay to shrink the particles and make them more translucent.
 class Particle 
   def initialize(sprite, x, y, ang, speed, decay)
     @x = x
@@ -27,52 +32,45 @@ class Particle
     @life = 1
     @sprite = sprite
     @decay = decay
+    
   end
   
   def update
     @x = @x + @d_x
     @y = @y + @d_y
     @life = @life - @decay
-    @angle += (@speed / 3)
-  end
-  
-  def color
-    
+    @angle += (@speed / 2)
   end
   
   def dead?
     @life <= 0
   end
   
+  def draw_layer(layer, color, x = nil, y = nil)
+    @sprite.draw_rot(x || @x, y || @y, layer, @angle, 0.5, 0.5, @life, @life, color, :default)    
+  end
+  
   def draw
-     @sprite.draw_rot(@x, 
-                  @y, 
-                  ZOrder::Fuzzy,
-                  @angle,
-                  0.5,
-                  0.5,
-                  @life, 
-                  @life, 
-                  Gosu::Color.new((@life * 255).floor, 255, 255, 255), 
-                  :default)
+    return if dead?
+    draw_layer(ZOrder::Shadow, Gosu::Color.new((@life * 128).floor,0,0,0), @x+2, @y+2)
+    draw_layer(ZOrder::Shape, Gosu::Color.new((@life * 255).floor, 255,255,0))
+    draw_layer(ZOrder::Highlight, Gosu::Color.new((@life * 20).floor,255,255,255))
   end
 end
 
+# This class keeps track of our moving particles. It loads in a few white png files, and removes
+# dead particles during every tick. They could probably be garbage collected less frequently.
 class Particles
   
+  SPRITES = ['leaf', 'leaf2', 'leaf3', 'star', 'fuzzy']
   def initialize(window)
     @particles = []
-    @fuzzy = Gosu::Image.new(window, "images/leaf.png", true)
+    @sprites = Particles::SPRITES.collect {|s| Gosu::Image.new(window, "images/#{s}.png", true)}
   end
   
   def update
-    to_remove = []
-    @particles.each do |p| 
-      p.update
-      to_remove << p if p.dead?
-    end
-    
-    to_remove.each {|r| @particles.delete(r)}
+    @particles.each {|p| p.update}
+    @particles.delete_if {|p| p.dead?}
   end
   
   def draw
@@ -80,13 +78,14 @@ class Particles
   end
   
   def create(x, y, angle, speed)
-    @particles << Particle.new(@fuzzy, x, y, angle, speed, 0.002)
+    @particles << Particle.new(@sprites[(rand * @sprites.size).floor], x, y, angle, speed, 0.01)
   end
   
 end
 
-class Emitter
-  
+# An emitter simply emits particles at a regular interval. In this case, every
+# time spawn is called, a new particle is generated with a random angle of movement and speed.
+class Emitter  
   def initialize(particles, x, y)
     @particles = particles
     @x = x
@@ -94,42 +93,38 @@ class Emitter
   end
   
   def spawn
-    @particles.create(@x, rand * Dimensions::Height, (rand * 30) + 165, (rand * 10))
-  end
-  def update(milliseconds)
-    spawn if (rand < 0.1) 
+    @particles.create(@x, @y, rand * 360, (rand * 3)+3)
   end
 end
-
-
 
 class GameWindow < Gosu::Window
   def initialize
     super(Dimensions::Width, Dimensions::Height, false)
-    self.caption = "Gosu Tutorial Game"
+    self.caption = "Particle Effects"
     
     @background_image = Gosu::Image.new(self, "images/background.png", true)
     @particles = Particles.new(self)
-    @emitter = Emitter.new(@particles, 1400, 400)
-    
+    @emitter = Emitter.new(@particles, Dimensions::Width / 2, Dimensions::Height / 2)
   end
 
+  # Called every tick by gosu to update object positions
   def update
-    @emitter.update(milliseconds)
+    @emitter.spawn
     @particles.update
   end
 
+  # Called every tick by gosu to draw things on the screen
   def draw
     @background_image.draw(0, 0, ZOrder::Background)
     @particles.draw
   end
-
+  
+  # Quit on ESC
   def button_down(id)
-    if id == Gosu::Button::KbEscape then
-      close
-    end
+    close if id == Gosu::Button::KbEscape
   end
 end
 
+# Create the gosu window
 window = GameWindow.new
 window.show
